c96ed9cf8d7137d77667f66e81e3b89bb0f19ea7
Raghu Vaidyanathan <ragstoriches919@gmail.com>
2021-12-01 17:38:32 -0500

    Finished getplayercoords

--- Battleship.java
+++ Battleship.java
@@ -12,23 +12,29 @@ public class Battleship {
 		System.out.println("Welcome to Battleship!");
 		System.out.println("PLAYER 1, ENTER YOUR SHIPS’ COORDINATES.");
 		
-		int[][] player1Coords = new int[5][2];
-		int[][] player2Coords = new int[5][2];
+		int[][] playerCoords1 = new int[5][2];
+		int[][] playerCoords2 = new int[5][2];
 		
-//		System.out.println(Arrays.deepToString(player1Coords));
-		Battleship.getPlayerCoords(player1Coords, input);
 		
-//		input.next();
+		playerCoords1 = Battleship.getPlayerCoords(playerCoords1, input, "PLAYER1");
+		playerCoords2 = Battleship.getPlayerCoords(playerCoords2, input, "PLAYER2");
+		
+		System.out.println(Arrays.deepToString(playerCoords1));
+		System.out.println(Arrays.deepToString(playerCoords2));
+		
 		
 	}
 	
 	
 	/**
 	 * Returns 2d array of player positions
-	 * @param playerArr - 2d array of integers 
+	 * @param playerArr - 2d array of integers
+	 * @param playerName - String ex.) "PLAYER 1"
 	 * @return 2d array of integers
 	 */
-	public static int[][] getPlayerCoords(int[][] playerArr, Scanner inputObj){
+	public static int[][] getPlayerCoords(int[][] playerArr, Scanner inputObj, String playerName){
+		
+		System.out.println(playerName + "ENTER YOUR SHIPS' COORDINATES.");
 		
 //		Loop through and add coordinates
 		for (int i = 0; i < playerArr.length; i++) {
@@ -39,21 +45,39 @@ public class Battleship {
 			int secondCoord = inputObj.nextInt();
 			int[] tempArr = new int[]{firstCoord, secondCoord};
 			
-			while ( (firstCoord >4 || firstCoord < 0) || (secondCoord >4 || secondCoord < 0) || Battleship.valueInBattleshipArray(playerArr, tempArr) ){
+			
+			// Keep looping if coords are outside of range
+			while ( (firstCoord >4 || firstCoord < 0) || (secondCoord >4 || secondCoord < 0)){
 				
 				System.out.println("Invalid coordinates. Choose different coordinates.");
 				System.out.println("Entire Ship " + (i+1) + " location:");
 				firstCoord = inputObj.nextInt();
 				secondCoord = inputObj.nextInt();
-				
-				 
-				
-				System.out.println("Entire Ship " + (i+1) + " location:");
-//				System.out.println(Battleship.valueInBattleshipArray(playerArr, tempArr))   ;
-			
+
+				tempArr[0] = firstCoord;
+				tempArr[1] = secondCoord;
 			}
 			
-//		    Add values to 2D array
+
+			boolean inArray = Battleship.valueInBattleshipArray(playerArr, tempArr);
+			
+			// Keep looking if coords have already been entered before.
+			while (inArray) {
+				System.out.println("You already have a ship there. Choose different coordinates.");
+				System.out.println("Entire Ship " + (i+1) + " location:");
+				firstCoord = inputObj.nextInt();
+				secondCoord = inputObj.nextInt();
+				
+				tempArr[0] = firstCoord;
+				tempArr[1] = secondCoord;
+				
+				inArray = Battleship.valueInBattleshipArray(playerArr, tempArr);
+			}
+			
+			
+			
+			
+		    // Add values to 2D array
 			playerArr[i][0] = firstCoord;
 			playerArr[i][1] = secondCoord;
 			
@@ -75,13 +99,36 @@ public class Battleship {
 		
 		for (int i = 0; i < playerArr.length; i++) {
 			
-			if (playerArr[i] == coord) {
+			if (playerArr[i][0] == coord[0] && playerArr[i][1] == coord[1] ) {
+				
 				return true;
 			}
 		}
 		return false;
-		
-		
 	}
+	
+	
+ // Use this method to print game boards to the console.
+	private static void printBattleShip(char[][] player) {
+		System.out.print("  ");
+		for (int row = -1; row < 5; row++) {
+			if (row > -1) {
+				System.out.print(row + " ");
+			}
+			for (int column = 0; column < 5; column++) {
+				if (row == -1) {
+					System.out.print(column + " ");
+				} else {
+					System.out.print(player[row][column] + " ");
+				}
+			}
+			System.out.println("");
+		}
+	}
+	
+	
+	
+	
+	
 
 }
